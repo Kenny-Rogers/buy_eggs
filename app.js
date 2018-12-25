fd227cd1951870dc436ff8e47bbc3e554b170589
kenny-rogers <ylodonu@gmail.com>
2018-12-25 19:20:38 +0000

    setting up routes

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 const express = require('express');
 //creating instance of express
 const app = express();
+//including path module
+const path = require('path');
+
 //defining templating engine as ejs
 app.set('view engine', 'ejs');
 
-//using a middleware that creates an alias for our static files
+//defining a middleware to handle request static files
 app.use('/public', express.static(path.join(__dirname, 'static')));
 
 //defining router for different routes
@@ -15,8 +18,16 @@ app.use('/admin', adminRouter);
 
 
 //defining general routes
-app.get('/', (request,response)=>{});
-app.get('/staff_login',(request,response)=>{});
+app.get('/', (request,response)=>{
+    response.write("welcome to homepage");
+    response.send();
+});
+
+app.get('/staff_login',(request,response)=>{
+    //displaying login page
+    response.render('login');
+});
+
 app.get('/staff_logout', (request, response) => { });
 
 
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,7 +2,55 @@
 const express = require('express');
 //creating an instance of the express router
 const adminRouter = express.Router();
+//using layouts module
+const ejsLayouts = require('express-ejs-layouts');
+//using nodemailer module
+const nodemailer = require('nodemailer');
 
+//mail arguments
+const transporter = nodemailer.createTransport({
+    service : 'gmail',
+    auth : {
+        user: 'transfast90@gmail.com',
+        pass: 'transFAST900!',
+    }
+});
+
+var mailOptions = {
+    from: 'transfast90@gmail.com',
+    to: 'ylodonu@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+
+//adding the layout to the router
+adminRouter.use(ejsLayouts);
+
+//route to /dashboard
+adminRouter.get('/dashboard',(request,response)=>{
+    response.render('dashboard',{data:{title:'Dashboard'}});
+});
+
+//route to handle /transactions/day
+adminRouter.get('/transactions/:day',(request,response)=>{
+    response.render('transaction_details', { data: { title: 'Transaction for ' + request.params.day}});
+});
+
+//route to handle /transactions/history
+adminRouter.get('/history_transactions', (request, response) => {
+    response.render('transaction_details', { data: { title: 'Transaction History'}});
+});
+
+//route to handle /daily_sales
+adminRouter.get('/daily_sales',(request, response)=>{
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    }); 
+});
 
 //making adminRouter global
 module.exports = adminRouter; 
